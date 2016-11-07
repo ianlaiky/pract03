4eaf08fef17d68dd0283a57958ae7713011c1788
152772a <152772a@mymail.nyp.edu.sg>
2016-11-07 15:13:42 +0800

    ini

---
+++ src/demo/BookDetailsServlet.java
@@ -0,0 +1,22 @@
+package demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by 152772A on 11/7/2016.
+ */
+@WebServlet(name = "BookDetailsServlet")
+public class BookDetailsServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
